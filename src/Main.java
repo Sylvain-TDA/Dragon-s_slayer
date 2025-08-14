95f32677f92cec07252499127b70c5a45aeb06ea
Sylvain <sylvain.teissier@le-campus-numerique.fr>
2025-08-14 09:52:41 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,99 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        List of classes needed
+
+        private class Menu{
+        }
+
+        private class Game{
+        }
+
+        private class Character{
+
+            private class Warrior {
+
+            name = ;
+            attack = ;
+            life = ;
+            }
+
+            private class Magician {
+            name = ;
+            attack = ;
+            life = ;
+            }
+        }
+
+        private class Square{
+
+        case du jeu
+        }
+
+        private class Weapon{
+        private Sword{
+            -> Warrior's attack +5
+            }
+
+            private Club{
+            -> Warrior's attack +3
+            }
+        }
+
+        private class Equipment{
+
+            private StandardPotion{
+            -> life +2
+            }
+
+            private BigPotion{
+            -> life +5
+            }
+
+        }
+
+        private class Spell {
+
+            private Fireball{
+            -> Magician's attack +7
+            }
+
+            private Lightning{
+            -> Magician's attack +2
+            }
+
+        }
+
+        private class Enemy{
+
+            private Sorcerer {
+                attack 2
+                life 9
+            }
+
+            private Goblin {
+                attack 1
+                life 6
+            }
+
+            private Dragon {
+                attack 4
+                life 15
+            }
+
+        }
+
+        private class Dice{
+        }
+
+        private class SurpriseChest{
+        }
+
+
+         */
+
+
+        }
+    }
+}
