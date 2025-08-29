f2e960c9746c8cd77a53fef398df47eaf0df15a3
Sylvain <sylvain.teissier@le-campus-numerique.fr>
2025-08-29 10:02:16 +0200

    ajout traitemetn exceptions

--- src/fr/dragonsslayer/Menu.java
+++ src/fr/dragonsslayer/Menu.java
@@ -41,6 +41,7 @@ public class Menu {
 
     /**
      * Getter for the name of the hero.
+     *
      * @return NullPointerException If no hero has been created.
      */
 
@@ -54,6 +55,7 @@ public class Menu {
 
     /**
      * Getter for the type of the hero.
+     *
      * @return NullPointerException If no hero has been created.
      */
 
@@ -68,6 +70,7 @@ public class Menu {
     /**
      * Request the player what type he wants to play, either Warrior or Magician.
      * The Scanner handle what the user entered.
+     *
      * @return the type selected under a specific format.
      */
 
@@ -80,6 +83,10 @@ public class Menu {
             displayMessage(typeSelection);
             userInput = keyboard.nextLine().trim();
 
+            if (userInput.isEmpty()) {
+                displayMessage("Vous devez entrer un type. Réessayez.");
+                continue;
+            }
             if (userInput.equalsIgnoreCase("Warrior") || userInput.equalsIgnoreCase("Magician")) {
                 return userInput.substring(0, 1).toUpperCase() + userInput.substring(1).toLowerCase();
             } else {
@@ -92,13 +99,25 @@ public class Menu {
     /**
      * Request the player to enter the name of his hero.
      * The Scanner handle what the user entered.
+     *
      * @return the name entered.
      */
 
     public String askName() {
-        String name = "Entrez le nom : ";
-        displayMessage(name);
-        return keyboard.nextLine();
+        String name;
+        while (true) {
+            displayMessage("Veuillez entrer un nom : ");
+            name = keyboard.nextLine().trim();
+            if (name.isEmpty()) {
+                displayMessage("Bien tenté ' '");
+            } else if (name.length() > 10) {
+                displayMessage("Un chasseur de dragon doit malheureusement raccourcir son nom.");
+            } else if (!name.matches("[a-zA-Z0-9 ]+")) { // Autorise lettres, chiffres et espaces
+                displayMessage("Le nom contient des caractères interdits. Réessayez.");
+            } else {
+                return name;
+            }
+        }
     }
 
     /**
@@ -133,9 +152,8 @@ public class Menu {
                     displayMessage(selectionText);
                 }
             } catch (InputMismatchException e) {
-                String errorText = "Entrée invalide. Veuillez entrer un nombre (1, 2 ou 3).";
-                displayMessage(errorText);
-                keyboard.next();
+                displayMessage("Entrée invalide. Veuillez entrer un nombre (1, 2 ou 3).");
+                keyboard.nextLine();
             }
         }
         return choice;
@@ -146,53 +164,59 @@ public class Menu {
      * Handle the creation of the hero, starting a game or quiting a game.
      *
      * @throws InterruptedException if the thread is interrupted during the game.
-     * @throws HeroOutOfTheBoardException if the hero goes out of the board.
      */
 
 
     public void mainMenu() throws InterruptedException {
-        boolean exit = false;
-        boolean playerCreated = false;
+        try {
+            boolean exit = false;
+            boolean playerCreated = false;
 
-        while (!exit) {
-            switch (displayMenu()) {
-                case 1: // Création personnage
-                    hero = createHero();
-                    manageHero(hero);
-                    playerCreated = true;
-                    break;
-                case 2: //Jouer
-                    if (playerCreated) {
+            while (!exit) {
+                switch (displayMenu()) {
+                    case 1: // Création personnage
+                        hero = createHero();
+                        manageHero(hero);
+                        playerCreated = true;
+                        break;
+                    case 2: //Jouer
+                        if (playerCreated) {
+                            try {
+                                game.startingAGame();
+                                game.playingTheGame();
+                            } catch (HeroOutOfTheBoardException e) {
+                                displayMessage(e.getMessage());
+                            }
+                        } else {
+                            String errorText = "Vous devez créer un personnage avant de vous lancer dans l'aventure";
+                            displayMessage(errorText);
+                        }
+                        break;
+                    case 3: // Quitter
+                        exit = true;
+                        String exitText = "Oh non... À toute !";
+                        displayMessage(exitText);
+                        break;
+                    case 4: // Cheat pour le test, écris un perso auto et lance le jeu
+
                         try {
+                            createCheatedHero();
                             game.startingAGame();
                             game.playingTheGame();
                         } catch (HeroOutOfTheBoardException e) {
                             displayMessage(e.getMessage());
                         }
-                    } else {
-                        String errorText = "Vous devez créer un personnage avant de vous lancer dans l'aventure";
-                        displayMessage(errorText);
-                    }
-                    break;
-                case 3: // Quitter
-                    exit = true;
-                    String exitText = "Oh non... À toute !";
-                    displayMessage(exitText);
-                    break;
-                case 4: // Cheat pour le test, écris un perso auto et lance le jeu
-
-                    try {
-                        createCheatedHero();
-                        game.startingAGame();
-                        game.playingTheGame();
-                    } catch (HeroOutOfTheBoardException e) {
-                        displayMessage(e.getMessage());
-                    }
-                    break;
-                default:
-                    String invalidChoice = "Choix invalide !";
-                    displayMessage(invalidChoice);
+                        break;
+                    default:
+                        String invalidChoice = "Choix invalide !";
+                        displayMessage(invalidChoice);
+                }
             }
+
+        } catch (
+                InterruptedException e) {
+            displayMessage("Le jeu a été interrompu.");
+            Thread.currentThread().interrupt();
         }
     }
 
@@ -248,30 +272,37 @@ public class Menu {
      */
 
     public void manageHero(Hero hero) throws InterruptedException {
+
         boolean play = false;
 
         while (!play) {
-            switch (displayCharacterMenu()) {
-                case 1:
-                    System.out.println(hero);
-                    break;
-                case 2:
-                    hero.modify(askType(), askName());
-                    break;
-                case 3:
-                    play = true;
-                    try {
-                        game.setBoard();
-                        game.getBoard();
-                        game.startingAGame();
-                        game.playingTheGame();
-                    } catch (HeroOutOfTheBoardException e) {
-                        displayMessage(e.getMessage());
-                    }
-                    break;
-                default:
-                    String invalidChoice = "Choix invalide !";
-                    displayMessage(invalidChoice);
+            try {
+                switch (displayCharacterMenu()) {
+                    case 1:
+                        System.out.println(hero);
+                        break;
+                    case 2:
+                        hero.modify(askType(), askName());
+                        break;
+                    case 3:
+                        play = true;
+                        try {
+                            game.setBoard();
+                            game.getBoard();
+                            game.startingAGame();
+                            game.playingTheGame();
+                        } catch (HeroOutOfTheBoardException e) {
+                            displayMessage(e.getMessage());
+                        } catch (Exception e) {
+                            displayMessage("Erreur lors du lancement du jeu : " + e.getMessage());
+                        }
+                        break;
+                    default:
+                        String invalidChoice = "Choix invalide !";
+                        displayMessage(invalidChoice);
+                }
+            } catch (Exception e) {
+                displayMessage("Erreur : " + e.getMessage());
             }
         }
     }
